896a2fd98ae8a2b66e656229f1e61eb30f3845e5
fe-treichert <thomas.reichert@funke-engineering.de>
2019-12-02 02:33:08 +0100

    enable to take screenshot

--- SIFT_feature_detection_openCV.py
+++ SIFT_feature_detection_openCV.py
@@ -2,23 +2,24 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-gray1 = cv2.imread ("Origami.jpg", cv2.IMREAD_GRAYSCALE)
+#gray1 = cv2.imread ("Origami.jpg", cv2.IMREAD_GRAYSCALE)
+
+video = cv2.VideoCapture(0)
+check, frame2 = video.read()
+orb = cv2.ORB_create(nfeatures=500)
 
 
-orb = cv2.ORB_create(nfeatures=2000)
-
-
+gray1 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 kpts1, des1  = orb.detectAndCompute(gray1, None)
 gray1 = cv2.drawKeypoints (gray1, kpts1, None)
 
 gray1_canny = cv2.Canny(gray1,threshold1=100,threshold2=200)
-plt.subplot(121),plt.imshow(gray1,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(gray1_canny,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+#plt.subplot(121),plt.imshow(gray1,cmap = 'gray')
+#plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+#plt.subplot(122),plt.imshow(gray1_canny,cmap = 'gray')
+#plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+#plt.show()
 
-video = cv2.VideoCapture(0)
 while True:    
     check, frame2 = video.read()
     if not check: 
@@ -43,10 +44,11 @@ while True:
     key = cv2.waitKey(1)
     if key == ord('q'):
        break
-
+    
+    if key == ord('n'):
+        gray1 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        kpts1, des1  = orb.detectAndCompute(gray1, None)
+        gray1 = cv2.drawKeypoints (gray1, kpts1, None)
 
 video.release()
-
-
 cv2.destroyAllWindows()
-
