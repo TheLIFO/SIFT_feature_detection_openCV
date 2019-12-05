c01261a189e90a787cc05c26281fe5d79747ad22
Thomas Reichert <thomas.reichert@funke-engineering.de>
2019-12-05 16:37:46 +0100

    various changes UI and bugfix camera

--- SIFT_feature_detection_openCV.py
+++ SIFT_feature_detection_openCV.py
@@ -1,8 +1,5 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-
-#gray1 = cv2.imread ("Origami.jpg", cv2.IMREAD_GRAYSCALE)
 
 video = cv2.VideoCapture(0)
 check, frame2 = video.read()
@@ -13,19 +10,15 @@ gray1 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 kpts1, des1  = orb.detectAndCompute(gray1, None)
 gray1 = cv2.drawKeypoints (gray1, kpts1, None)
 
-gray1_canny = cv2.Canny(gray1,threshold1=100,threshold2=200)
-#plt.subplot(121),plt.imshow(gray1,cmap = 'gray')
-#plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-#plt.subplot(122),plt.imshow(gray1_canny,cmap = 'gray')
-#plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-#plt.show()
-
-while True:    
+while True:        
     check, frame2 = video.read()
+    cv2.imshow("live", frame2)
+    cv2.waitKey(10)
+
     if not check: 
         break
     gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow("Capturing",gray2)
+    
 
     kpts2, des2  = orb.detectAndCompute(gray2, None)
     gray2 = cv2.drawKeypoints (gray2, kpts2, None)
@@ -37,11 +30,11 @@ while True:
     matches = sorted (matches, key = lambda x:x.distance)
 
     matches = [m for m in matches if m.distance<60]
-    matching_result = cv2.drawMatches(gray1, kpts1, gray2, kpts2, matches, None, flags=2)
+    matching_result = cv2.drawMatches(gray1, kpts1, gray2, kpts2, matches[:10], None, flags=2)
     cv2.imshow("Results", matching_result)
 
     
-    key = cv2.waitKey(1)
+    key = cv2.waitKey(10)
     if key == ord('q'):
        break
     
---
+++ start.bat
@@ -0,0 +1,3 @@
+cd C:\Users\ThomasReichert\source\repos\TheLIFO\SIFT_feature_detection_openCV
+call C:\ProgramData\Anaconda3\Scripts\activate ocv
+python.exe SIFT_feature_detection_openCV.py
